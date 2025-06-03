c6389b26babe36de8ad5e685d4603c3dbf0706c9
Andrés Tarazona <44829958+tarazonaa@users.noreply.github.com>
2025-06-03 17:32:06 -0600

    Auth db (#9)
    * Setup auth tables
    
    ---------
    
    Co-authored-by: Alejandro Arouesty <117224520+AleAro21@users.noreply.github.com>

--- db/seed.sql
+++ db/seed.sql
@@ -2,3 +2,17 @@
 INSERT INTO users (username, email, password)
     VALUES ('tarazonaa', pgp_sym_encrypt('someemail@gmail.com', 'SOME_PASSWORD'), pgp_sym_encrypt('123', 'SOME_PASSWORD'));
 
+INSERT INTO images (user_id, sent_image_id, received_image_id)
+    VALUES ((
+            SELECT
+                id
+            FROM
+                users
+            WHERE
+                username = 'tarazonaa'), gen_random_uuid (), gen_random_uuid ()),
+    ((
+        SELECT
+            id
+        FROM users
+        WHERE
+            username = 'tarazonaa'), gen_random_uuid (), gen_random_uuid ());
