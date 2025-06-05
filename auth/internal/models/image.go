1abaf2c59aa9ffa2d83ebf1dcda3e5621b18f460
AleAro <aarouesty@pastpost.com>
2025-06-04 18:55:44 -0600

    image API for managing sent/received image UUIDs

---
+++ auth/internal/models/image.go
@@ -0,0 +1,32 @@
+package models
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
+
+type Image struct {
+	ID              uuid.UUID `json:"id" gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
+	UserID          uuid.UUID `json:"user_id" gorm:"not null;type:uuid"`
+	SentImageID     uuid.UUID `json:"sent_image_id" gorm:"uniqueIndex;not null;type:uuid"`
+	ReceivedImageID uuid.UUID `json:"received_image_id" gorm:"uniqueIndex;not null;type:uuid"`
+	CreatedAt       time.Time `json:"created_at"`
+
+	
+	User User `json:"user,omitempty" gorm:"foreignKey:UserID"`
+}
+
+func (i *Image) BeforeCreate(tx *gorm.DB) error {
+	if i.ID == uuid.Nil {
+		i.ID = uuid.New()
+	}
+	if i.SentImageID == uuid.Nil {
+		i.SentImageID = uuid.New()
+	}
+	if i.ReceivedImageID == uuid.Nil {
+		i.ReceivedImageID = uuid.New()
+	}
+	return nil
+}
